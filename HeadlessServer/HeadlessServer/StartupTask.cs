bc98a78e23d156b732d106a3c0676d563bba1836
Petrus De Klerk <petrus.deklerk@sybrin.com>
2017-02-28 15:26:38 +0200

    Initial commit. Added blinky and begining of webserver. Link for webserver to continue on : https://sandervandevelde.wordpress.com/2016/04/11/passing-data-between-windows-10-iot-core-headed-and-headless-apps/
    Will update readme too.

---
+++ HeadlessServer/HeadlessServer/StartupTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.System.Threading;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace HeadlessServer
+{
+    public sealed class StartupTask : IBackgroundTask {
+        private static BackgroundTaskDeferral _Deferral = null;
+
+        public async void Run(IBackgroundTaskInstance taskInstance) {
+            _Deferral = taskInstance.GetDeferral();
+
+            var webserver = new MyWebServer();
+
+            await ThreadPool.RunAsync(workItem =>
+            {
+                webserver.Start();
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RaspberryPi
 Some Code Projects for raspberry Pi I am working on.
+
+
+https://sandervandevelde.wordpress.com/2016/04/11/passing-data-between-windows-10-iot-core-headed-and-headless-apps/
